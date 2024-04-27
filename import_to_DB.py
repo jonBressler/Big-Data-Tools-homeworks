623980b6da6f6e9a8073b400e87f16157822bce7
Jonathan Bressler <jonathanbressler@jonathans-mbp.mynetworksettings.com>
2024-04-27 16:58:59 -0400

    Updating folder area

---
+++ import_to_DB.py
@@ -0,0 +1,3 @@
+# https://www.mongodb.com/docs/database-tools/mongoimport/
+clusterString = 'mongodb+srv://jonbressler1895:<password>@finalprojectcluster.g9zftpp.mongodb.net/?retryWrites=true&w=majority&appName=FinalProjectCluster'
+mongoimport clusterString Sales_Records.csv
