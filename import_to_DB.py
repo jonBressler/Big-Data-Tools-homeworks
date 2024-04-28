01785092e1fbf844facf87e326eaa3cc6a534160
Jonathan Bressler <jonathanbressler@jonathans-mbp.mynetworksettings.com>
2024-04-27 23:25:28 -0400

    Updating import_to_DB and added PushToGit

--- import_to_DB.py
+++
@@ -1,3 +0,0 @@
-# https://www.mongodb.com/docs/database-tools/mongoimport/
-clusterString = 'mongodb+srv://jonbressler1895:<password>@finalprojectcluster.g9zftpp.mongodb.net/?retryWrites=true&w=majority&appName=FinalProjectCluster'
-mongoimport clusterString Sales_Records.csv
